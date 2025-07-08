0405eab0bb894398cb9740519bd28aa14989dabe
nath-sam2 <as20240628@sci.sjp.ac.lk>
2025-07-08 09:07:20 +0530

    Initial commit

---
+++ main.c
@@ -0,0 +1,10 @@
+#include <stdio.h>
+#include <stdlib.h>
+
+int main()
+{
+    printf("First git hub Project\n");
+    printf("Revolutionaries wait for my head on a silver plate.\n");
+    printf("Coldplay Viva la vida");
+    return 0;
+}
